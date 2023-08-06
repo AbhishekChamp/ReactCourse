29989e8d793d3558b6cdeabbfb0b9fe9fcc8f80a
AbhishekChamp <abhishek96.r@gmail.com>
2023-08-06 22:40:19 +0530

    Day - 36

--- 25-fast-react-pizza/src/features/menu/MenuItem.jsx
+++ 25-fast-react-pizza/src/features/menu/MenuItem.jsx
@@ -1,18 +1,36 @@
-function MenuItem({ pizza }) {
-  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+import PropTypes from "prop-types";
+import { formatCurrency } from "../../utils/helpers";
 
-  return (
-    <li>
-      <img src={imageUrl} alt={name} />
-      <div>
-        <p>{name}</p>
-        <p>{ingredients.join(', ')}</p>
-        <div>
-          {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
-        </div>
-      </div>
-    </li>
-  );
+function MenuItem({ pizza }) {
+    const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+
+    return (
+        <li>
+            <img src={imageUrl} alt={name} />
+            <div>
+                <p>{name}</p>
+                <p>{ingredients.join(", ")}</p>
+                <div>
+                    {!soldOut ? (
+                        <p>{formatCurrency(unitPrice)}</p>
+                    ) : (
+                        <p>Sold out</p>
+                    )}
+                </div>
+            </div>
+        </li>
+    );
 }
 
 export default MenuItem;
+
+MenuItem.propTypes = {
+    pizza: PropTypes.shape({
+        id: PropTypes.number,
+        name: PropTypes.string,
+        unitPrice: PropTypes.number,
+        ingredients: PropTypes.array,
+        soldOut: PropTypes.bool,
+        imageUrl: PropTypes.string,
+    }),
+};
