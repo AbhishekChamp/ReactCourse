99e43d92e724955c1ab2430a50651c9c57077fc9
AbhishekChamp <abhishek96.r@gmail.com>
2023-08-09 00:51:37 +0530

    Day - 39

--- 25-fast-react-pizza/src/features/order/OrderItem.jsx
+++ 25-fast-react-pizza/src/features/order/OrderItem.jsx
@@ -1,18 +1,29 @@
+import PropTypes from "prop-types";
 import { formatCurrency } from "../../utils/helpers";
 
 function OrderItem({ item, isLoadingIngredients, ingredients }) {
-    const { quantity, name, totalPrice } = item;
+  const { quantity, name, totalPrice } = item;
 
-    return (
-        <li>
-            <div>
-                <p>
-                    <span>{quantity}&times;</span> {name}
-                </p>
-                <p>{formatCurrency(totalPrice)}</p>
-            </div>
-        </li>
-    );
+  return (
+    <li className="py-3">
+      <div className="flex items-center justify-between gap-4 text-sm">
+        <p>
+          <span className="font-bold">{quantity}&times;</span> {name}
+        </p>
+        <p className="font-bold">{formatCurrency(totalPrice)}</p>
+      </div>
+    </li>
+  );
 }
 
 export default OrderItem;
+
+OrderItem.propTypes = {
+  item: PropTypes.shape({
+    quantity: PropTypes.number,
+    name: PropTypes.string,
+    totalPrice: PropTypes.number,
+  }),
+  isLoadingIngredients: PropTypes.bool,
+  ingredients: PropTypes.any,
+};
